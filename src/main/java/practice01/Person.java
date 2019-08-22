bc229626340d5d9e5894727dc3459a2817d970f9
sdp <403213485@qq.com>
2019-08-22 22:53:08 +0800

    1

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,12 @@
 package practice01;
 
 public class Person {
+	public String name;
+	public int age;
+	
+
+	public String introduce() {
+		return String.format("My name is %s. I am %d years old.", this.name, this.age);
+	}
+	
 }
